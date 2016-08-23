745247753fb31cc7b76d701c7b5a96f16b8e6524
Elimar Riesebieter <riesebie@lxtec.de>
2016-08-23 20:13:28 +0200

    Applied 0006-debian-specific-use_usr_bin_editor.patch

--- init.c
+++ init.c
@@ -3385,7 +3385,7 @@ void mutt_init (int skip_sys_rc, LIST *commands)
   {
     p = getenv ("EDITOR");
     if (!p)
-      p = "vi";
+      p = "/usr/bin/editor";
   }
   Editor = safe_strdup (p);
   Visual = safe_strdup (p);
